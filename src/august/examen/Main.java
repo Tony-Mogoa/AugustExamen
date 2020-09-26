7df537c6e1d20cd12e3e85eecd1f86fa686dcc12
Tony-Mogoa <mogoa.tonny@gmail.com>
2020-09-26 11:29:34 +0300

    Renamed smaple.fxml to login.fxml

--- src/august/examen/Main.java
+++ src/august/examen/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("layouts/sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("layouts/login.fxml"));
         primaryStage.setTitle("August Examen");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
